7627f2480f5f2215446347987b0401037c1faf84
teetzMcveets <119613936+teetzMcveets@users.noreply.github.com>
2023-05-25 12:26:03 +0100

    navigation styles

--- src/shared/Navigation/Navigation/Navigation.js
+++ src/shared/Navigation/Navigation/Navigation.js
@@ -7,8 +7,38 @@ export default function Navigation () {
     return (
         <nav className='nav'>
 
-            <div className='main-logo-container'>
+            {/* LOGO CONTAINER */}
+            <div>
                 <img src={img} alt='Ctrecs Logo' />
+            </div>
+
+            {/* NAVIGATION LINKS */}
+            <ul className='nav-ul'>
+                <NavLink to='#' className='nav-link'>
+                    <i className="fas fa-chart-pie icon-size"></i>
+                </NavLink>
+                <NavLink to='#' className='nav-link'>
+                    <i className="far fa-calendar-alt icon-size"></i>
+                </NavLink>
+                <NavLink to='#' className='nav-link'>
+                    <i className="fas fa-user icon-size"></i>
+                </NavLink>
+                <NavLink to='#' className='nav-link'>
+                    <i className="fas fa-handshake icon-size"></i>
+                </NavLink>
+                <NavLink to='#' className='nav-link'>
+                    <i className="fas fa-hourglass-start icon-size"></i>
+                </NavLink>
+                <NavLink to='#' className='nav-link'>
+                    <i className="fas fa-chart-line icon-size"></i>
+                </NavLink>
+            </ul>
+
+            {/* SETTINGS LINK */}
+            <div className='nav-settings-container'>
+                <Link to='#' className='settings-link'>
+                    <i className='fas fa-cog'></i>
+                </Link>
             </div>
         </nav>
     )
