b41be63bed0f4c6f247e0c5e3800f9b9cafc6a81
teetzMcveets <119613936+teetzMcveets@users.noreply.github.com>
2023-06-04 09:56:12 +0100

    medical questions started

--- src/components/Candidates/CandidateAll/CandidateDetails/CandidatePages/MedicalQuestions/MedicalQuestions.js
+++ src/components/Candidates/CandidateAll/CandidateDetails/CandidatePages/MedicalQuestions/MedicalQuestions.js
@@ -4,46 +4,61 @@ export default function MedicalQuestions ({ candidate, edit }) {
 
     return (
         <>
-            <div style={{ display: 'flex' }}>
-                <div>
-                    <div className='edit-button-container'>
-                        <div>
-                            <button 
-                                className='button-secondary'
-                                onClick={edit}
-                            >
-                                <i className='fas fa-pencil-alt'></i>
-                            </button>
-                        </div>
-                    </div>
+            <div className='candidate-page-left-container'>
+                <div className='candidate-page-button-container'>
+                    <button 
+                        className='button-secondary'
+                        onClick={edit}
+                    >
+                        <i className='fas fa-pencil-alt'></i>
+                    </button>
                 </div>
-                <div>
+            </div> 
+
+            <div className='candidate-page-right-candidate'>
+                <div className='candidate-page-full-page'>
+                
                     <div className='card-detail-title'>
                         <div className='card-title'>
                             Medical Questions
                         </div>
                     </div>
 
-                    <div className='card-container'>
+                    <div className='candidate-page-card-container'>
 
-                            <div className='questionnaire-title'>
-                                Do you Suffer any of the following - 
-                            </div>
-                        
-                        <div className='medical-detail-container'>
-                            <div className='detail-label'>
+                        <div className='candidate-page-detail-container'>
+                            Do you suffer from any of these conditions?
+                        </div>
+
+                        <div>
+                            <div>
                                 Diabetes
                             </div>
 
-                            <div className='detail-answer'>
-                                <div className='answer-border'>
-                                    {candidate.diabetes}
+                            <div>
+                                <div>
+                                    <input 
+                                        type='radio'
+                                    />
+                                    <label className='candidate-page-rad-label'>
+                                        Yes
+                                    </label>
+                                </div>
+                                <div>
+                                    <input 
+                                        type='radio'
+                                    />
+                                    <label className='candidate-page-rad-label'>
+                                        No
+                                    </label>
                                 </div>
                             </div>
                         </div>
+
+
                     </div>
                 </div>
-            </div>    
+            </div>
         </>
     )
 }
