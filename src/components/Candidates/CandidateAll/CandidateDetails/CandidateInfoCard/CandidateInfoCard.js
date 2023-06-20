d426c22f0825b0c65b8f07689e4f35cf284e769e
teetzMcveets <119613936+teetzMcveets@users.noreply.github.com>
2023-06-20 23:14:19 +0100

    back to candidates button added

--- src/components/Candidates/CandidateAll/CandidateDetails/CandidateInfoCard/CandidateInfoCard.js
+++ src/components/Candidates/CandidateAll/CandidateDetails/CandidateInfoCard/CandidateInfoCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './CandidateInfoCard.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CandidateInfoCard ({ candidate }) {
+    const navigate = useNavigate();
+
+    const handleOnBackClick = () => {
+        navigate('/candidates')
+    }
 
     return (
         <>
             <div className='info-card'>
-
+                <div style={{ position: 'fixed', left: '80px' }}>
+                    <button onClick={handleOnBackClick} className='button-primary'>
+                        Back to candidates...
+                    </button>
+                </div>
 
                 <div className='info-container'>
                     <p style={{ display: 'flex', justifyContent: 'center', fontSize: 14 }}>{candidate.firstName} {candidate.lastName}</p>
